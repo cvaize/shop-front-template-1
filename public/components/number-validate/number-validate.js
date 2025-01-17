aba067cd9167be924b7f8abb4ad48dbce88c62ee
cvaize <cvaize@gmail.com>
2025-01-17 15:56:48 +0500

    init

--- public/components/number-validate/number-validate.js
+++ public/components/number-validate/number-validate.js
@@ -10,10 +10,17 @@
         numElement.addEventListener('input', validate);
         numElement.addEventListener('keyup', validate);
         numElement.addEventListener('paste', validate);
+        validate.call(numElement, true);
     }
 
-    function validate() {
-        if (this.min) this.value = Math.max(parseInt(this.min), parseInt(this.value) || 0);
-        if (this.max) this.value = Math.min(parseInt(this.max), parseInt(this.value) || 0);
+    function validate(event, isSetCaret) {
+        let caret = this.selectionStart;
+        if (this.min) this.value = Math.max(parseInt(this.min), parseInt(String(this.value).replace(/\D/g, "")) || 0);
+        if (this.max) this.value = Math.min(parseInt(this.max), parseInt(String(this.value).replace(/\D/g, "")) || 0);
+        if (this.value) this.value = Number(this.value).toLocaleString();
+        if (!isSetCaret) {
+            this.setSelectionRange(caret, caret);
+            this.selectionStart = caret;
+        }
     }
 })();
